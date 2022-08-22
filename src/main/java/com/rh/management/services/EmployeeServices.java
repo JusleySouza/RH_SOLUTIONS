fcb144a61a7edde6f4208a23fa7166e8880a92d1
Jusley Souza <jusleysouza31@gmail.com>
2022-08-22 09:46:21 -0300

    Creating EmployeeServices

---
+++ src/main/java/com/rh/management/services/EmployeeServices.java
@@ -0,0 +1,24 @@
+package com.rh.management.services;
+
+import java.util.List;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+
+import com.rh.management.model.Employee;
+import com.rh.management.model.dto.EmployeeDTO;
+import com.rh.management.model.dto.EmployeeSearchDTO;
+
+@Service
+public interface EmployeeServices {
+
+
+	ResponseEntity<Employee> create(EmployeeDTO employee);
+	
+	List<Employee> search(EmployeeSearchDTO employeeSearch);
+	
+	Employee edit(Long id);
+	
+	ResponseEntity<Employee> update(Employee employee);
+	
+}
