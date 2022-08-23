68ca49c075601024df52986144e12f5d1265e2ac
Jusley Souza <jusleysouza31@gmail.com>
2022-08-23 19:59:06 -0300

    Creation of the compensation model

--- src/main/java/com/rh/management/model/Compensation.java
+++ src/main/java/com/rh/management/model/Compensation.java
@@ -3,12 +3,25 @@ package com.rh.management.model;
 import java.util.Date;
 
 import javax.persistence.Column;
+import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
 
 import org.springframework.format.annotation.DateTimeFormat;
 
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Entity
+@Getter
+@Setter
+@ToString
+@EqualsAndHashCode
 public class Compensation {
 
 	@Id
@@ -22,11 +35,14 @@ public class Compensation {
 	@Column(name="amount", nullable= false)
 	private double amount;
 	
-	@Column(name="description", nullable= false)
+	@Column(name="description", nullable= true)
 	private String description;
 	
 	@Column(name="date", nullable= false)
 	@DateTimeFormat(pattern = "yyyy-MM")
 	private Date date;
-	
+//	
+//	@ManyToOne
+//	@JoinColumn(name = "employee_id")
+//	private Employee employee;
 }
