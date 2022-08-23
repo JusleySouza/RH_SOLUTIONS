9348234acdabffeaeb98b97d8975a121d4f521d5
Jusley Souza <jusleysouza31@gmail.com>
2022-08-23 20:02:01 -0300

    Updating the date template

--- src/main/java/com/rh/management/model/dto/EmployeeDTO.java
+++ src/main/java/com/rh/management/model/dto/EmployeeDTO.java
@@ -3,6 +3,8 @@ package com.rh.management.model.dto;
 import java.text.ParseException;
 import java.time.LocalDate;
 
+import org.springframework.format.annotation.DateTimeFormat;
+
 import com.rh.management.model.Employee;
 
 import lombok.EqualsAndHashCode;
@@ -19,6 +21,7 @@ public class EmployeeDTO {
 	private String firstName;
 	private String middleName;
 	private String lastName;
+	@DateTimeFormat(pattern = "yyyy-MM-dd")
 	private LocalDate birthDate;
 	private String position;
 	
