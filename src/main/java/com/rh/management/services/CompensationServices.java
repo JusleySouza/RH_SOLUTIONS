0e0a072c87b58d8cd5b7abb6395cc10248b12426
Jusley Souza <jusleysouza31@gmail.com>
2022-08-26 10:30:00 -0300

    Creation of the compensation service and the create method.

---
+++ src/main/java/com/rh/management/services/CompensationServices.java
@@ -0,0 +1,14 @@
+package com.rh.management.services;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+
+import com.rh.management.model.Compensation;
+import com.rh.management.model.dto.CompensationDTO;
+
+@Service
+public interface CompensationServices {
+
+	ResponseEntity<Compensation> create(CompensationDTO compensation);
+	
+}
