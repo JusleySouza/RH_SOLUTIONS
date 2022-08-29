a57d5b054fca8efb0f0336e3c258a40103e84e1e
Jusley Souza <jusleysouza31@gmail.com>
2022-08-29 20:12:27 -0300

    Adding the employee object

--- src/main/java/com/rh/management/model/Compensation.java
+++ src/main/java/com/rh/management/model/Compensation.java
@@ -7,6 +7,7 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.ManyToOne;
 
 import org.springframework.format.annotation.DateTimeFormat;
 
@@ -39,8 +40,7 @@ public class Compensation {
 	@Column(name="date", nullable= false)
 	@DateTimeFormat(pattern = "yyyy-MM")
 	private Date date;
-//	
-//	@ManyToOne
-//	@JoinColumn(name = "employee_id")
-//	private Employee employee;
+
+	@ManyToOne
+	private Employee employee;
 }
