1cb2107c1894325831e1ddc1fbdccd1f2346e535
Jusley Souza <jusleysouza31@gmail.com>
2022-08-29 20:10:03 -0300

    Adding the findByEmployee method

--- src/main/java/com/rh/management/repository/CompensationRepository.java
+++ src/main/java/com/rh/management/repository/CompensationRepository.java
@@ -1,9 +1,13 @@
 package com.rh.management.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.rh.management.model.Compensation;
+import com.rh.management.model.Employee;
 
 public interface CompensationRepository extends JpaRepository<Compensation, Long> {
 
+	List<Compensation> findByEmployee(Employee employee);
 }
