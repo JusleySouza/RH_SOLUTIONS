d28ab1dd0cea5256c7d73367054528e0f7dfb376
Jusley Souza <jusleysouza31@gmail.com>
2022-08-29 22:51:39 -0300

    creation of the findByEmployee andDateBetween method

--- src/main/java/com/rh/management/repository/CompensationRepository.java
+++ src/main/java/com/rh/management/repository/CompensationRepository.java
@@ -1,5 +1,6 @@
 package com.rh.management.repository;
 
+import java.util.Date;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -10,4 +11,5 @@ import com.rh.management.model.Employee;
 public interface CompensationRepository extends JpaRepository<Compensation, Long> {
 
 	List<Compensation> findByEmployee(Employee employee);
+	List<Compensation> findByEmployeeAndDateBetween(Employee employee, Date dataInicio, Date dataFim);
 }
