b06273e883b57f6087372b4140fcddf76a05dae1
Jusley Souza <jusleysouza31@gmail.com>
2022-09-02 20:19:41 -0300

    Changing the method to List

--- src/main/java/com/rh/management/services/CompensationServices.java
+++ src/main/java/com/rh/management/services/CompensationServices.java
@@ -1,6 +1,5 @@
 package com.rh.management.services;
 
-import java.util.Date;
 import java.util.List;
 
 import org.springframework.http.ResponseEntity;
@@ -8,12 +7,13 @@ import org.springframework.stereotype.Service;
 
 import com.rh.management.model.Compensation;
 import com.rh.management.model.dto.CompensationDTO;
+import com.rh.management.model.dto.CompensationSearchDTO;
 
 @Service
 public interface CompensationServices {
 
 	ResponseEntity<Compensation> create( Long id, CompensationDTO compensation);
 	
-	List<Compensation> display( Long id, Date dataInicio, Date dataFim);
+	List<Compensation> display( Long id, CompensationSearchDTO compensation);
 
 }
