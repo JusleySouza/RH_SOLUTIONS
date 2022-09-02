4fef31b887bc608008f8d2028df69e32d9b5f9e3
Jusley Souza <jusleysouza31@gmail.com>
2022-09-02 20:47:11 -0300

    Adding DateTimeFormat

--- src/main/java/com/rh/management/model/dto/CompensationSearchDTO.java
+++ src/main/java/com/rh/management/model/dto/CompensationSearchDTO.java
@@ -2,6 +2,8 @@ package com.rh.management.model.dto;
 
 import java.util.Date;
 
+import org.springframework.format.annotation.DateTimeFormat;
+
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.Setter;
@@ -13,7 +15,9 @@ import lombok.ToString;
 @EqualsAndHashCode
 public class CompensationSearchDTO {
 
+	@DateTimeFormat(pattern = "yyyy-MM")
 	private Date dataInicio;
+	@DateTimeFormat(pattern = "yyyy-MM")
 	private Date dataFim;
 	
 }
